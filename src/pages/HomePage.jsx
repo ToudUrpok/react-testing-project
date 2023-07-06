842506476ebb89c53843bc1fa351a1836e8f0d99
y.yakubovich <y.yakubovich@softteco.com>
2023-07-06 09:54:37 +0300

    Added pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div data-testid='home-page'>
+            HOME PAGE
+        </div>
+    );
+};
+
+export default HomePage;
